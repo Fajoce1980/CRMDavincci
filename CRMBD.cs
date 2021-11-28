643d4529a564b4f55220f0bedabb2c312e56cb0e
Fajoce1980 <94017271+Fajoce1980@users.noreply.github.com>
2021-11-28 17:28:26 -0500

    Add files via upload

---
+++ CRMBD.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Control_Pagos.Logica
+{
+    class CRMBD
+    {
+        public SqlConnection Conexion = new SqlConnection(@"Data Source=.\SQLEXPRESS;Initial Catalog=DavincciCRM;User ID=sa;Password=12345");
+        public SqlConnection AbrirConexion()
+        {
+            if (Conexion.State == ConnectionState.Closed)
+                Conexion.Open();
+            return Conexion;
+        }
+        public SqlConnection CerrarConexion()
+        {
+            if (Conexion.State == ConnectionState.Open)
+                Conexion.Close();
+            return Conexion;
+        }
+    }
+}
